33592132595668f8f2a35aa210c5ca1c3d0e1bf9
Greg Leuch <contact@gleu.ch>
2014-10-22 18:02:11 -0400

    allow getting today's color data from listener.

--- lib/listen.rb
+++ lib/listen.rb
@@ -40,17 +40,27 @@ loop do
       params = Addressable::URI.parse(req.gsub(rgx, 'http://localhost/?\1')).query_values rescue nil
       raise 'InvalidParams' unless params.present?
 
-      # Base64 unencoded unescaped url string
-      decodedUrl = Addressable::URI::unescape(Addressable::URI.unencode(Base64.decode64(params['url']))) rescue nil
-      raise 'InvalidUrl' if decodedUrl.blank?
+      if params['color'].present?
+        ctype = 'text/plain'
+        d = params['days'] rescue nil
+        d ||= 1
+        obj = BrowseHistory.days(d)
 
-      # Attempt to add to browse history
-      hist = BrowseHistory.add(decodedUrl, params['ip'])
-      raise 'NotSaved' unless hist.present?
+        {hex_color: obj.avg_hex_color, days: d, pages_count: obj.count, pages_with_color_count: obj.joins(:web_page).where("#{WebPage.table_name}.colored = ?",true).count}
+        
+      else
+        # Base64 unencoded unescaped url string
+        decodedUrl = Addressable::URI::unescape(Addressable::URI.unencode(Base64.decode64(params['url']))) rescue nil
+        raise 'InvalidUrl' if decodedUrl.blank?
 
-      puts "Added: #{hist.web_page.url}"
+        # Attempt to add to browse history
+        hist = BrowseHistory.add(decodedUrl, params['ip'])
+        raise 'NotSaved' unless hist.present?
 
-      {success: true, url: params['url']}
+        puts "Added: #{hist.web_page.url}"
+
+        {success: true, url: params['url']}
+      end
 
     rescue => err
       {success: false, message: err.to_s}
@@ -59,7 +69,7 @@ loop do
     resp = resp.to_json
 
     # Return request status
-    client.puts ["HTTP/1.1 200 OK","Date: #{Time.now.strftime('%a, %d %b %Y %H:%M:%S %Z')}","Server: MyInternetColor/1.0","Content-Type: image/png; charset=utf-8","Content-Length: #{resp.length}\r\n\r\n"].join("\r\n")
+    client.puts ["HTTP/1.1 200 OK","Date: #{Time.now.strftime('%a, %d %b %Y %H:%M:%S %Z')}","Server: MyInternetColor/1.0","Content-Type: #{ctype || 'image/png'}; charset=utf-8","Content-Length: #{resp.length}\r\n\r\n"].join("\r\n")
     client.puts resp
     client.close
   end
